3fd44ed3daf6727b6492992ce6c4f0aeb14e3449
Ferg <uberferg@gmail.com>
2017-11-13 03:38:52 -0500

    added awesome installation and compositing setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -23,30 +23,32 @@ ln ~/conf/.tmux.conf ~/
 mkdir -p ~/packages
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/packages/tmux-resurrect
 
+# load gnome-terminal profiles into dconf
+#dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < Red.dconf
+
 
 #############
 # Unity setup
 #############
 
 # set wallpaper
-gsettings set org.gnome.desktop.background picture-uri file:/home/$(whoami)/conf/wallpapers/brooklyn_red_cityscape-other.jpg
-# load gnome-terminal profiles into dconf
-#TODO test this
-#dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < Red.dconf
+#gsettings set org.gnome.desktop.background picture-uri file:/home/$(whoami)/conf/wallpapers/brooklyn_red_cityscape-other.jpg
 
 
 ###############
 # Awesome setup
 ###############
 
-#TODO set up awesome
-
-# install awesome
-sudo apt install awesome
+# install awesome and a compositing manager
+sudo apt update
+sudo apt install xcompmgr awesome
+rm ~/.xprofile
+ln -s ~/conf/.xprofile ~/.xprofile
 
 # copy awesome config files
 sudo cp -r ~/conf/redferg /usr/share/awesome/themes
 sudo ln -fs ~/conf/rc.lua /etc/xdg/awesome/rc.lua
+
 
 # restart Xorg server
 sudo pkill X
