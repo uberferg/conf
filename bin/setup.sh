43cc5b7e60766a8fc8c40398fc105f25b8ba0e6f
Ferg <uberferg@gmail.com>
2017-11-29 11:40:59 -0500

    updated setup script to setup urxvt and to use ln -f instead of manual rm of files

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,51 +3,47 @@
 #TODO add "Are you sure" check before deting files
 
 # link bashrc and bash_aliases
-rm ~/.bash_aliases ~/.bashrc
-ln ~/conf/.bash_aliases ~/
-ln ~/conf/.bashrc ~/
+ln -f ~/conf/.bashrc ~/
+ln -f ~/conf/.bash_aliases ~/
 
 # link git files
-rm ~/.gitconfig
-ln ~/conf/.gitconfig ~/
+ln -f ~/conf/.gitconfig ~/
 
 # link vim files
-rm ~/.vimrc
-rm -rf ~/.vim
-ln ~/conf/.vimrc ~/
-ln -s ~/conf/.vim ~/
+ln -f ~/conf/.vimrc ~/
+ln -fs ~/conf/.vim ~/
 
 # link tmux files and download tmux-resurrect
-rm ~/.tmux.conf
-ln ~/conf/.tmux.conf ~/
+ln -f ~/conf/.tmux.conf ~/
 mkdir -p ~/packages
 git clone https://github.com/tmux-plugins/tmux-resurrect ~/packages/tmux-resurrect
-
-# load gnome-terminal profiles into dconf
-#dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < Red.dconf
 
 
 #############
 # Unity setup
 #############
-
 # set wallpaper
 #gsettings set org.gnome.desktop.background picture-uri file:/home/$(whoami)/conf/wallpapers/brooklyn_red_cityscape-other.jpg
+
+# load gnome-terminal profiles into dconf
+#dconf load /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/ < Red.dconf
 
 
 ###############
 # Awesome setup
 ###############
+# install urxvt and link Xresources file (color scheme for urxvt)
+sudo apt update
+sudo apt install rxvt-unicode
+ln -f ~/.Xresources ~/
 
 # install awesome and a compositing manager
-sudo apt update
 sudo apt install xcompmgr awesome
-rm ~/.xprofile
-ln -s ~/conf/.xprofile ~/.xprofile
+ln -f ~/conf/.xprofile ~/.xprofile
 
 # copy awesome config files
 sudo cp -r ~/conf/redferg /usr/share/awesome/themes
-sudo ln -fs ~/conf/rc.lua /etc/xdg/awesome/rc.lua
+sudo ln -f ~/conf/rc.lua ~/.config/awesome/
 
 
 # restart Xorg server
